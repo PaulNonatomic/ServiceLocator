f163197024f268b3345b07e18652aea04c0b244a
Paul Stamp <paul@nonatomic.co.uk>
2024-11-25 20:28:56 +0000

    Amended promises to return instantly

--- Runtime/ServicePromise.cs
+++ Runtime/ServicePromise.cs
@@ -5,14 +5,52 @@ namespace Nonatomic.ServiceLocator
 {
 	public class ServicePromise<T> : IServicePromise<T>
 	{
-		private readonly TaskCompletionSource<T> _taskCompletion = new TaskCompletionSource<T>();
+		private T _result;
+		private Exception _error;
+		private bool _isResolved;
+		private bool _isRejected;
+		private readonly TaskCompletionSource<T> _taskCompletion = new();
 
-		public void Resolve(T value) => _taskCompletion.TrySetResult(value);
-		public void Reject(Exception ex) => _taskCompletion.TrySetException(ex);
+		public void Resolve(T value)
+		{
+			if (_isResolved || _isRejected) return;
+			_result = value;
+			_isResolved = true;
+			_taskCompletion.TrySetResult(value);
+		}
+
+		public void Reject(Exception ex)
+		{
+			if (_isResolved || _isRejected) return;
+			_error = ex;
+			_isRejected = true;
+			_taskCompletion.TrySetException(ex);
+		}
 
 		public ServicePromise<TResult> Then<TResult>(Func<T, TResult> onFulfilled)
 		{
 			var resultPromise = new ServicePromise<TResult>();
+
+			if (_isResolved)
+			{
+				try
+				{
+					var result = onFulfilled(_result);
+					resultPromise.Resolve(result);
+				}
+				catch (Exception ex)
+				{
+					resultPromise.Reject(ex);
+				}
+				return resultPromise;
+			}
+
+			if (_isRejected)
+			{
+				resultPromise.Reject(_error);
+				return resultPromise;
+			}
+
 			_taskCompletion.Task.ContinueWith(task =>
 			{
 				if (task.IsCompletedSuccessfully)
@@ -32,12 +70,34 @@ namespace Nonatomic.ServiceLocator
 					resultPromise.Reject(task.Exception);
 				}
 			});
+
 			return resultPromise;
 		}
-		
+
 		public ServicePromise<T> Then(Action<T> onFulfilled)
 		{
 			var resultPromise = new ServicePromise<T>();
+
+			if (_isResolved)
+			{
+				try
+				{
+					onFulfilled(_result);
+					resultPromise.Resolve(_result);
+				}
+				catch (Exception ex)
+				{
+					resultPromise.Reject(ex);
+				}
+				return resultPromise;
+			}
+
+			if (_isRejected)
+			{
+				resultPromise.Reject(_error);
+				return resultPromise;
+			}
+
 			_taskCompletion.Task.ContinueWith(task =>
 			{
 				if (task.IsCompletedSuccessfully)
@@ -57,12 +117,34 @@ namespace Nonatomic.ServiceLocator
 					resultPromise.Reject(task.Exception ?? new Exception("Task failed."));
 				}
 			});
+
 			return resultPromise;
 		}
-		
+
 		public ServicePromise<T> Catch(Action<Exception> onRejected)
 		{
 			var resultPromise = new ServicePromise<T>();
+
+			if (_isRejected)
+			{
+				try
+				{
+					onRejected(_error);
+					resultPromise.Resolve(default);
+				}
+				catch (Exception ex)
+				{
+					resultPromise.Reject(ex);
+				}
+				return resultPromise;
+			}
+
+			if (_isResolved)
+			{
+				resultPromise.Resolve(_result);
+				return resultPromise;
+			}
+
 			_taskCompletion.Task.ContinueWith(task =>
 			{
 				if (task.IsFaulted)
@@ -82,6 +164,7 @@ namespace Nonatomic.ServiceLocator
 					resultPromise.Resolve(task.Result);
 				}
 			});
+
 			return resultPromise;
 		}
 	}
